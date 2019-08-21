e35194ca397947a54f433723b56cb82046fc9871
isaac_manubag <isaac_manubag@yahoo.com>
2019-08-21 20:49:08 +0800

    updates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { BannerComponent } from './banner/banner.component';
+import { ShortenerComponent } from './shortener/shortener.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BannerComponent,
+    ShortenerComponent
   ],
   imports: [
     BrowserModule
